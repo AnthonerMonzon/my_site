e885e19bba2ef7cbd50e60b82c9fa12dec03c718
AnthonerMonzon <puppz03@gmail.com>
2024-06-12 04:22:15 +0800

    Recommit

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.starting_page, name="starting-page"),
-    path("posts", views.posts, name="posts-page"),    
-    path("posts/<slug>", views.post_details, name="post-details-page")# /posts/my-posts/my-on-post
+    path("skills", views.posts, name="posts-page"),    
+    path("skills/<slug>", views.post_details, name="post-details-page")# /posts/my-posts/my-on-post
 ]
