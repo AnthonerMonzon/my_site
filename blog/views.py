70f0e6ed7c11c98a8d19fd2d957b0fa312d11721
AnthonerMonzon <puppz03@gmail.com>
2024-06-12 03:55:33 +0800

    Adding Single Post Page, 404 Page

--- blog/views.py
+++ blog/views.py
@@ -24,6 +24,7 @@ all_skills = [
         "excerpt": "PHP code is executed on the server. PHP: Hypertext Preprocessor.",
         "content": """
         PHP is a general-purpose scripting language geared towards web development. It was originally created by Danish-Canadian programmer Rasmus Lerdorf in 1993 and released in 1995. The PHP reference implementation is now produced by the PHP Group. PHP was originally an abbreviation of Personal Home Page, but it now stands for the recursive initialism PHP: Hypertext Preprocessor.
+        
         PHP code is usually processed on a web server by a PHP interpreter implemented as a module, a daemon or a Common Gateway Interface (CGI) executable. On a web server, the result of the interpreted and executed PHP code—which may be any type of data, such as generated HTML or binary image data—would form the whole or part of an HTTP response. Various web template systems, web content management systems, and web frameworks exist that can be employed to orchestrate or facilitate the generation of that response. Additionally, PHP can be used for many programming tasks outside the web context, such as standalone graphical applications and drone control.[16] PHP code can also be directly executed from the command line.
         """
     },
@@ -37,7 +38,23 @@ all_skills = [
         "content": """
         Delphi is a general-purpose programming language and a software product that uses the Delphi dialect of the Object Pascal programming language and provides an integrated development environment (IDE) for rapid application development of desktop, mobile, web, and console software, currently developed and maintained by Embarcadero Technologies. 
         Delphi's compilers generate native code for Microsoft Windows, macOS, iOS, Android and Linux (x64).
+        
         Delphi includes a code editor, a visual designer, an integrated debugger, a source code control component, and support for third-party plugins. The code editor features Code Insight (code completion), Error Insight (real-time error-checking), and refactoring. The visual forms designer has the option of using either the Visual Component Library (VCL) for pure Windows development or the FireMonkey (FMX) framework for cross-platform development. Database support is a key feature and is provided by FireDAC (Database Access Components). Delphi is known for its fast compilation speed, native code, and developer productivity.
+        """
+    },
+    {
+        "slug": "csharp-programming",
+        "image": "csharp.png",
+        "author": "ner",
+        "date": date(2024, 6, 11),
+        "title": "C#.Net",
+        "excerpt": "C# is a modern, innovative, open-source, cross-platform object-oriented programming.",
+        "content": """
+        C# is a modern, innovative, open-source, cross-platform object-oriented programming language and one of the top 5 programming languages on GitHub. 
+        
+        Do you have experience with JavaScript, Java, or C++? You'll find C# instantly familiar, and you'll enjoy its evolving features including type safety, generics, pattern matching, async, records, and more. 
+        
+        We hope you'll fall in love with C# from the very first keystroke.
         """
     }
 ]
@@ -58,8 +75,13 @@ def starting_page(request):
 
 
 def posts(request):
-    return render(request, "blog/all-skills.html")
+    return render(request, "blog/all-skills.html", {
+        "skills": all_skills
+    })
 
 
 def post_details(request, slug):
-    return render(request, "blog/skill-detail.html")
+    indetified_skill = next(skill for skill in all_skills if skill['slug'] == slug)
+    return render(request, "blog/skill-detail.html", {
+        "skill" : indetified_skill
+    })
